13f71451e232d1576521965b75870056d4f9967f
Dobby <godelbit@gmail.com>
2024-10-20 15:15:51 +0300

    add about us component

---
+++ client/src/app/modules/about-us/about-us/about-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+/**
+ * Component that represents the About Us page of the application.
+ * This component is responsible for rendering the content and layout of the About Us page.
+ */
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.scss',
+})
+export class AboutUsComponent {}
