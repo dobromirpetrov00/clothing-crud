cecf554393a8d43a26500fa7c17cff6daa233963
Dobby <godelbit@gmail.com>
2024-10-20 15:15:10 +0300

    add footer component

---
+++ client/src/app/layout/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * The `FooterComponent` is a standalone Angular component that represents the footer of the application.
+ * It is responsible for rendering the footer content and any associated functionality.
+ */
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
